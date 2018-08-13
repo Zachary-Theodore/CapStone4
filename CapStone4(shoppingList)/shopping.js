9aecaf8df0f7acf6700de381a03e9657686dc4c2
Zachary_Theodore <fk9836@wayne.edu>
2018-08-13 16:45:45 -0400

    JavaScript stuff

---
+++ CapStone4(shoppingList)/shopping.js
@@ -0,0 +1,33 @@
+﻿
+var itemName = "";
+var itemPrice = 0;
+var receipt = []
+function displayClick(choice)
+{
+    
+    var items = ["Milk", "Eggs", "Cereal", "Juice", "Butter", "Cheese"]
+    var price = [1.98, 3.40, 4.83, 2.99, 4.00, 7.89]
+
+    receipt.push(items[choice]);
+
+
+    //console.log(items[choice]);
+
+    itemPrice = itemPrice + price[choice];
+    
+    //console.log(itemPrice);
+}
+function checkOut()
+{
+    
+    for (var i = 0; i < receipt.length; i++)
+   {
+    console.log(receipt[i]);
+    
+
+    }
+    console.log(itemPrice.toFixed(2));
+   
+    
+
+}
